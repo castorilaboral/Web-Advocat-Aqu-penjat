8458daabf1bf4ad671a661678b9d5f9524575d60
castorilaboral <castorilaboral@gmail.com>
2024-12-10 11:07:28 +0100

    hola

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,12 +5,14 @@ import "./globals.css";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
-  weight: "100 900",
+  weight: "400 700",
+  display: "swap",
 });
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
-  weight: "100 900",
+  weight: "400 700",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
